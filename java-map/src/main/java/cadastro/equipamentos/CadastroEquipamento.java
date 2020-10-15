b64f912ae2da107b6505f2fd9cc7f0aa20481cff
Robson Duarte <robson.o.d@gmail.com>
2020-10-15 13:12:00 -0300

    examplo de uso do java map

---
+++ java-map/src/main/java/cadastro/equipamentos/CadastroEquipamento.java
@@ -0,0 +1,22 @@
+package cadastro.equipamentos;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CadastroEquipamento {
+
+    // esse cara e o "Banco"
+    private Map<String, Equipamento> estoque = new HashMap<>();
+
+    // exemplo de como salvar
+    public void salvar(Equipamento equipamento){
+        estoque.put(equipamento.getCodigo(), equipamento);
+    }
+
+    // exemplo de como pesquisar
+    public Equipamento pesquisar(String codigo){
+        return estoque.get(codigo);
+    }
+
+    // demais operacões... aqui
+}
---
+++ java-map/src/main/java/cadastro/equipamentos/Equipamento.java
@@ -0,0 +1,29 @@
+package cadastro.equipamentos;
+
+public class Equipamento {
+
+    private String codigo;
+    private String nome;
+
+    public Equipamento(String codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
