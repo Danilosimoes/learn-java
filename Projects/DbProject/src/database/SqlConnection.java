be30dfd7b0c412423cc8b3c9685da01a4c619520
danilosimoes <danilossimoes28@gmail.com>
2020-12-18 19:33:05 -0300

    updating project

--- Projects/DbProject/src/database/SqlConnection.java
+++ Projects/DbProject/src/database/SqlConnection.java
@@ -10,24 +10,15 @@ public class SqlConnection {
 
     public static Connection getSql()  {
         try {
-            Properties prop = getProperties();
-            final String url = prop.getProperty("db.url");
-            final String user = prop.getProperty("db.user");
-            final String password = prop.getProperty("db.password");
+            final String url = "*";
+            final String user = "*";
+            final String password = "*";
 
             return DriverManager.getConnection(url, user, password);
 
-        }  catch (SQLException | IOException e){
+        } catch (SQLException e){
             throw new RuntimeException(e);
         }
 
-
-
-    }
-    private static Properties getProperties() throws IOException {
-        Properties prop = new Properties();
-        String cnt = "/connection.properties";
-        prop.load(SqlConnection.class.getResourceAsStream(cnt));
-        return prop;
     }
 }
